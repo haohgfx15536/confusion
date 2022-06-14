f83731d6736a68a9f16f812940a088b62e77701e
hao huynh <haohgfx15536@funix.edu.vn>
2022-06-14 22:42:47 +0700

    React Component Part 2

--- src/Components/MenuComponent.js
+++ src/Components/MenuComponent.js
@@ -1,83 +1,59 @@
 import React, { Component } from 'react';
-import { Media } from 'reactstrap';
+import { Card, CardBody, CardImg, CardImgOverlay, CardText, CardTitle } from 'reactstrap';
 
 class Menu extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            dishes: [
-                {
-                    id: "0",
-                    name: "Uthappizza",
-                    image: "assets/images/uthappizza.png",
-                    category: "main",
-                    label: "hot",
-                    price: "4.99",
-                    description: "A unique combination of Indian Uthappam (Panceke and Itanlian pizza, topped with Cerignola olives, rip vine cherry tomatoes, Vidalia onion,, Guntur chillies and Buffalo Panner"
-                },
-                {
-                    id: "1",
-                    name: "Uthappizza",
-                    image: "assets/images/vadonut.png",
-                    category: "main",
-                    label: "hot",
-                    price: "4.99",
-                    description: "A unique combination of Indian Uthappam (Panceke and Itanlian pizza, topped with Cerignola olives, rip vine cherry tomatoes, Vidalia onion,, Guntur chillies and Buffalo Panner"
-                },
-                {
-                    id: "2",
-                    name: "Uthappizza",
-                    image: "assets/images/buffet.png",
-                    category: "main",
-                    label: "hot",
-                    price: "4.99",
-                    description: "A unique combination of Indian Uthappam (Panceke and Itanlian pizza, topped with Cerignola olives, rip vine cherry tomatoes, Vidalia onion,, Guntur chillies and Buffalo Panner"
-                },
-                {
-                    id: "3",
-                    name: "Uthappizza",
-                    image: "assets/images/elaicheesecake.png",
-                    category: "main",
-                    label: "hot",
-                    price: "4.99",
-                    description: "A unique combination of Indian Uthappam (Panceke and Itanlian pizza, topped with Cerignola olives, rip vine cherry tomatoes, Vidalia onion,, Guntur chillies and Buffalo Panner"
-                },
-                {
-                    id: "4",
-                    name: "Uthappizza",
-                    image: "assets/images/zucchipakoda.png",
-                    category: "main",
-                    label: "hot",
-                    price: "4.99",
-                    description: "A unique combination of Indian Uthappam (Panceke and Itanlian pizza, topped with Cerignola olives, rip vine cherry tomatoes, Vidalia onion,, Guntur chillies and Buffalo Panner"
-                },
-            ]
+            selectedDish: null
+        }
+
+        this.renderSelectedDish = this.renderSelectedDish.bind(this);
+
+    }
+
+    onSelectedDish(dish) {
+        this.setState({
+            selectedDish: dish
+        })
+    }
+
+    renderSelectedDish(dish) {
+        if (dish !== null)
+            return (
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.description} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            )
+        else {
+            return <div></div>
         }
     }
 
     render() {
-        const menu = this.state.dishes.map((dish) => {
+        const menu = this.props.dishes.map((dish) => {
             return (
-                <Media key={dish.id} tag="li" className="col-12 mt-5">
-                    <Media left middle>
-                        <Media object src={dish.image} alt={dish.description} />
-                    </Media>
-                    <Media body className="ml-5">
-                        <Media heading>{dish.name}</Media>
-                        <p>{dish.description}</p>
-                    </Media>
-                </Media>
+                <Card onClick={() => this.onSelectedDish(dish)} key={dish.id} className="col-12 col-md-5 mt-1">
+                    <CardImg width="100%" src={dish.image} alt={dish.description} />
+                    <CardImgOverlay>
+                        <CardTitle>{dish.name}</CardTitle>
+                    </CardImgOverlay>
+                </Card>
             )
         })
         return (
             <div className='container'>
                 <div className="row">
-                    <Media list>
-                        {menu}
-                    </Media>
+                    {menu}
                 </div>
+                {this.renderSelectedDish(this.state.selectedDish)}
             </div>
+
         )
     }
 }
---
+++ src/shared/dishes.js
@@ -0,0 +1,49 @@
+const DISHES = [
+    {
+        id: "0",
+        name: "Uthappizza",
+        image: "assets/images/uthappizza.png",
+        category: "main",
+        label: "hot",
+        price: "4.99",
+        description: "A unique combination of Indian Uthappam (Panceke and Itanlian pizza, topped with Cerignola olives, rip vine cherry tomatoes, Vidalia onion,, Guntur chillies and Buffalo Panner"
+    },
+    {
+        id: "1",
+        name: "Uthappizza",
+        image: "assets/images/vadonut.png",
+        category: "main",
+        label: "hot",
+        price: "4.99",
+        description: "A unique combination of Indian Uthappam (Panceke and Itanlian pizza, topped with Cerignola olives, rip vine cherry tomatoes, Vidalia onion,, Guntur chillies and Buffalo Panner"
+    },
+    {
+        id: "2",
+        name: "Uthappizza",
+        image: "assets/images/buffet.png",
+        category: "main",
+        label: "hot",
+        price: "4.99",
+        description: "A unique combination of Indian Uthappam (Panceke and Itanlian pizza, topped with Cerignola olives, rip vine cherry tomatoes, Vidalia onion,, Guntur chillies and Buffalo Panner"
+    },
+    {
+        id: "3",
+        name: "Uthappizza",
+        image: "assets/images/elaicheesecake.png",
+        category: "main",
+        label: "hot",
+        price: "4.99",
+        description: "A unique combination of Indian Uthappam (Panceke and Itanlian pizza, topped with Cerignola olives, rip vine cherry tomatoes, Vidalia onion,, Guntur chillies and Buffalo Panner"
+    },
+    {
+        id: "4",
+        name: "Uthappizza",
+        image: "assets/images/zucchipakoda.png",
+        category: "main",
+        label: "hot",
+        price: "4.99",
+        description: "A unique combination of Indian Uthappam (Panceke and Itanlian pizza, topped with Cerignola olives, rip vine cherry tomatoes, Vidalia onion,, Guntur chillies and Buffalo Panner"
+    },
+]
+
+export default DISHES;
